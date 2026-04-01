cc25ec3ad47c423fa0fef4a25a2b57c539785757
Gioele Cairo <cqt470@student.edu.ti.ch>
2026-04-01 21:55:57 +0200

    Aggiornate configurazioni

--- src/configs.py
+++ src/configs.py
@@ -1,5 +1,11 @@
 import os
 
-class configs:
+class Configs:
     def __init__(self):
-        pass
+        self.__base_path = os.path.realpath(f"{__file__}/../")
+        
+        # I path sono relativi a ~/src/
+        self.paths = {
+            "log_dir": f"{self.__base_path}\\logs\\",
+            "log_file": f"{self.__base_path}\\logs\\.log",
+        }
