36f9739223556877f1ed8398d612971adb676def
= <gioele.cairo@samtrevano.ch>
2026-04-14 13:50:27 +0200

    .

--- src/encrypt/encrypter.py
+++ src/encrypt/encrypter.py
@@ -2,13 +2,14 @@ from utils.logger import Logger
 from utils.file import File
 from cryptography.fernet import Fernet
 
-# https://stackoverflow.com/questions/66218337/encrypt-and-protect-file-with-python
+# https://www.geeksforgeeks.org/python/encrypt-and-decrypt-files-using-python/
 
 class Encrypter:
     def __init__(self):
         self.logger = Logger()
         self.key = Fernet.generate_key()
         self.cipher = Fernet(self.key)
+        self.key_directory = ""
 
     def encrypt_file(self, file_location):
         self.logger.log(f"Encrypting file: {file_location}")
--- src/utils/file.py
+++ src/utils/file.py
@@ -17,3 +17,12 @@ class File:
     def append(self, content):
         with open(self.path, "a") as f:
             f.write(f"{content}\n")
+            f.close()
+
+    def read(self):
+        with open(self.path, "r") as f:
+            return f.read()
+
+    def read_binary(self):
+        with open(self.path, "rb") as f:
+            return f.read()
